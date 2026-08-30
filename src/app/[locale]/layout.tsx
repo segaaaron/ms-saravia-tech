7c750de200cf8abe67c750e14ee40137246d5dc5
Miguel Angel Saravia <msaravia@ia.com.mx>
2026-08-29 22:10:15 -0400

    fix error

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from 'next'
 import { Space_Grotesk, Inter } from 'next/font/google'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
-// @ts-expect-error - Next.js provides CSS module typings at runtime/build time.
-import '../globals.css'
+import './globals.css'
 import Analytics from '@/components/seo/Analytics'
 import LazyMotionProvider from '@/components/fx/LazyMotionProvider'
 
